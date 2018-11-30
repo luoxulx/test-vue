064b9ed30d690832ccea692d47c3f0fb054015f6
luoxu <>
2018-11-30 23:15:17 +0800

    guard for web&api-a

--- src/views/layout/components/Navbar.vue
+++ src/views/layout/components/Navbar.vue
@@ -82,12 +82,12 @@ export default {
       this.$store.dispatch('toggleSideBar')
     },
     logout() {
-      logout().then(() => {
-        this.$message.success('logout successful')
-        this.$router.push('/login')
-        // this.$store.dispatch('LogOut').then(() => {
-        //   location.reload()// In order to re-instantiate the vue-router object to avoid bugs
-        // })
+      this.$store.dispatch('LogOut').then(() => {
+        location.reload()// In order to re-instantiate the vue-router object to avoid bugs
+        logout().then(() => {
+          this.$message.success('logout successful')
+          this.$router.push('/login')
+        })
       })
     }
   }
